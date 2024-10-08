93b0e039d1ca12df6e6a88c8656afe97a8794d0b
dvdai63 <dvdai63@gmail.com>
2024-10-08 21:13:57 +0700

    improve algorithm

--- d20241008.rb
+++ d20241008.rb
@@ -4,28 +4,25 @@
 # @return {Integer}
 def remove_duplicates(nums)
   index_uniq = 0
-  for i in 1..(nums.length-1) do
-    if nums[i] == nums[index_uniq]
-      nums[i] = '_'
-    else
+  (1..nums.length-1).each do |i|
+    if nums[i] != nums[index_uniq]
       index_uniq+=1
       nums[index_uniq] = nums[i]
       nums[i] = '_'
     end
   end
-  nums
+  index_uniq+1
 end
 
 require 'rspec'
 RSpec.describe 'remove_duplicates' do
   it 'removes duplicates from a sorted array' do
     nums = [1, 1, 2]
-    expect(remove_duplicates(nums)).to eq([1,2, '_'])
+    expect(nums.first(remove_duplicates(nums))).to eq([1,2])
   end
   it 'removes duplicates from a sorted array' do
     nums = [0,0,1,1,1,2,2,3,3,4]
-    out_nums = [0,1,2,3,4, '_','_','_','_','_']
-    expect(nums.size).to eq(out_nums.size)
-    expect(remove_duplicates(nums)).to eq(out_nums)
+    out_nums = [0,1,2,3,4]
+    expect(nums.first(remove_duplicates(nums))).to eq(out_nums)
   end
 end
